b8de7e8980762eefcd0051d7eb93a8a59277b68c
Xenower <1025761182alan@gmail.com>
2024-10-21 18:57:34 -0500

    time entries

---
+++ app/Models/Time_entrie.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Time_entrie extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nombre_entrada',
+        'hora_inicio',
+        'hora_final',
+        'total_horas',
+        'etiqueta',
+        'proyecto',
+        'usuario',
+        'fecha_creacion',
+        'fecha_modificacion'
+    ];
+}
